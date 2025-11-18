1bae5902d9be58fde42027342c22c2b590f1deb8
Manuel-SanClemente <a24manuelda@iessanclemente.net>
2025-11-18 13:28:39 +0100

    Initial

--- app/src/main/java/com/example/calculoimc/ImcCalculatorFragment.kt
+++ app/src/main/java/com/example/calculoimc/ImcCalculatorFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.findNavController
 
 class ImcCalculatorFragment : Fragment() {
     override fun onCreateView(
@@ -13,5 +15,13 @@ class ImcCalculatorFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_imc_calculator, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val button = view.findViewById<Button>(R.id.boton_calcular)
+        button.setOnClickListener {
+            view.findNavController().navigate(R.id.action_imcCalculatorFragment_to_resultImcFragment)
+        }
     }
 }
